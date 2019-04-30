230fcb206453b0ac6434f8bf969da01ecb5ff3af
Helena Rosenzweig <helena.rosenzw@gmail.com>
2019-04-30 11:27:58 +0200

    Added additional comments, up to video 4

--- app.js
+++ app.js
@@ -15,10 +15,13 @@ app.use('/client',express.static(__dirname + '/client'));
 serv.listen(2000);
 console.log("Server started");
 
-
+// Socket list keeps track of all clients connected to the server. 
 var SOCKET_LIST = {};
+
+// Player list keeps track of all players.
 var PLAYER_LIST = {}; 
 
+// Create a player, passes id as parameter
 var Player = function(id) {
     var self = {
         x:250,
@@ -32,6 +35,7 @@ var Player = function(id) {
         maxSpd: 10,
     }
 
+    // Will be called every frame
     self.updatePosition = function() {
         if(self.pressingRight)
             self.x+=self.maxSpd;
@@ -51,35 +55,48 @@ var io = require('socket.io')(serv,{});
 
 // Whenever a connection is established, the following function will be called.
 io.sockets.on('connection',function(socket){
+    // server assigns a unique id to the socket
     socket.id=Math.random();
+
+    // sets the initial x and y coordinates to 0
     socket.x=0;
     socket.y=0; 
+
+    // Every socket will have a random number between 0 and 1, will distinguish the different players. 
     socket.number = "" + Math.floor(10*Math.random());
+    
+    // Add it to the list of sockets currently online
     SOCKET_LIST[socket.id] = socket;
 
+    // Also create a player with the socket id, and add to player list
     var player = Player(socket.id);
     PLAYER_LIST[socket.id] = player; 
     
+    // Server listens to disconnects, and removes disconnected clients.
     socket.on('disconnect',function(){
         delete SOCKET_LIST[socket.id]; 
         delete PLAYER_LIST[socket.id];
     });
 
+    // Listens to client key presses, updates states of client accordingly
     socket.on('keyPress',function(data){
         if(data.inputId ==='left'){
-            player.pressingLeft= data.state; 
+            player.pressingLeft = data.state; 
         }else if(data.inputId ==='right'){
-            player.pressingRight= data.state; 
+            player.pressingRight = data.state; 
         }else if(data.inputId ==='up'){
-            player.pressingUp= data.state; 
+            player.pressingUp = data.state; 
         }else if(data.inputId ==='down'){
-            player.pressingDown= data.state; 
+            player.pressingDown = data.state; 
         }
     });
 
 });
 
+// Loops through every player in our player list, and will update the X and Y position.
 setInterval(function(){
+
+    // pack contains information about every single player in the game, and will be sent to every player conncted
     var pack = [];
     for(var i in PLAYER_LIST){
         var player = PLAYER_LIST[i];
@@ -87,14 +104,15 @@ setInterval(function(){
         pack.push({
             x:player.x,
             y:player.y,
-            number:player.number
+            number:player.number // player number added to package
         });
        
     }
+    // Server emits the pack to each  connected client
     for(var i in SOCKET_LIST){
         var socket = SOCKET_LIST[i];
-
-        socket.emit('newPositions',pack)
+        socket.emit('newPositions',pack);
     }
-   
+
+// The game will run at 25 frames per second.   
 },1000/25);
