37909bccc7de8c8c0790517999931c45af2ce12b
Helena Rosenzweig <helena.rosenzw@gmail.com>
2019-05-01 04:24:08 +0200

    Classes created for Entities and Player, bugs fixed. Functioning now as up to step 4

--- app.js
+++ app.js
@@ -19,9 +19,10 @@ console.log("Server started");
 var SOCKET_LIST = {};
 
 // Player list keeps track of all players.
-var PLAYER_LIST = {}; 
+// *** var PLAYER_LIST = {}; 
 
 var POINTS_LIST = {};
+
 
 // Constructor for keeping track of points in current game session
 var Points = function(){
@@ -39,63 +40,72 @@ var Points = function(){
     }
 }
 
-// Create a player, passes id as parameter
-var Player = function(id) {
+var Entity = function(){
     var self = {
         x:250,
         y:250,
-        id:id,
-        number:"" + Math.floor(10*Math.random()),
-        pressingRight:false,
-        pressingLeft:false,
-        pressingUp:false,
-        pressingDown: false,
-        maxSpd: 10,
+        spdX:0,
+        spdY:0,
+        id:""
+    }
+    self.update = function(){
+        self.updatePosition();
+    }
+    self.updatePosition = function(){
+        self.x += self.spdX;
+        self.y += self.spdY;
+    }
+    return self;
+}
+
+
+// Create a player, passes id as parameter
+var Player = function(id) {
+    var self = Entity();
+    self.id = id; 
+    self.number = "" + Math.floor(10*Math.random());
+    self.pressingRigh = false;
+    self.pressingLeft = false;
+    self.pressingUp = false;
+    self.pressingDown = false;
+    self.maxSpd = 10;
+    
+    var super_update = self.update;
+    self.update = function(){
+        self.updateSpd();
+        super_update();
     }
 
-    // Will be called every frame
-    self.updatePosition = function() {
-        if(self.pressingRight)
-            self.x+=self.maxSpd;
-        if(self.pressingLeft)
-            self.x-=self.maxSpd;
-        if(self.pressingUp)
-            self.y-= self.maxSpd;
-        if(self.pressingDown)
-            self.y += self.maxSpd;
+    self.updateSpd = function(){
+        if(self.pressingRight){
+            self.spdX = self.maxSpd;
+        }
+        else if(self.pressingLeft){
+            self.spdX = -self.maxSpd;
+        }
+        else{
+            self.spdX=0;
+        }
+
+        if(self.pressingDown){
+            self.spdY = self.maxSpd;
+        }
+        else if(self.pressingUp){
+            self.spdY = -self.maxSpd;
+        }
+        else{
+            self.spdY=0;
+        }
     }
+    Player.list[id] = self;
     return self; 
    
 }
+Player.list = {};
 
-// Initializes an io Socket object 
-var io = require('socket.io')(serv,{}); 
-
-// Whenever a connection is established, the following function will be called.
-io.sockets.on('connection',function(socket){
-    // server assigns a unique id to the socket
-    socket.id=Math.random();
-
-    // sets the initial x and y coordinates to 0
-    socket.x=0;
-    socket.y=0; 
-
-    // Every socket will have a random number between 0 and 1, will distinguish the different players. 
-    socket.number = "" + Math.floor(10*Math.random());
-    
-    // Add it to the list of sockets currently online
-    SOCKET_LIST[socket.id] = socket;
-
-    // Also create a player with the socket id, and add to player list
+Player.onConnect = function(socket){
+    console.log("hello");
     var player = Player(socket.id);
-    PLAYER_LIST[socket.id] = player; 
-    
-    // Server listens to disconnects, and removes disconnected clients.
-    socket.on('disconnect',function(){
-        delete SOCKET_LIST[socket.id]; 
-        delete PLAYER_LIST[socket.id];
-    });
-
     // Listens to client key presses, updates states of client accordingly
     socket.on('keyPress',function(data){
         if(data.inputId ==='left'){
@@ -108,24 +118,61 @@ io.sockets.on('connection',function(socket){
             player.pressingDown = data.state; 
         }
     });
+}
 
+Player.onDisconnect = function(socket){
+    delete Player.list[socket.id];
+}
+
+Player.update = function(){
+    var pack = [];
+    for(var i in Player.list){
+        var player = Player.list[i];
+        player.update();
+        pack.push({
+            x:player.x,
+            y:player.y,
+            number:player.number
+        });
+    }
+    return pack;
+}
+
+var Car = function(){
+    var self = Entity();
+    self.id = Math.random();
+    self.spdX = 0;
+    self.spdY = 30;
+}
+
+
+// Initializes an io Socket object 
+var io = require('socket.io')(serv,{}); 
+
+// Whenever a connection is established, the following function will be called.
+io.sockets.on('connection',function(socket){
+    // server assigns a unique id to the socket
+    socket.id=Math.random();
+    // Add it to the list of sockets currently online
+    SOCKET_LIST[socket.id] = socket;
+   
+    Player.onConnect(socket);
+    
+    // Server listens to disconnects, and removes disconnected clients.
+    socket.on('disconnect',function(){
+        delete SOCKET_LIST[socket.id]; 
+        Player.onDisconnect(socket);
+    });
 });
 
 // Loops through every player in our player list, and will update the X and Y position.
 setInterval(function(){
 
     // pack contains information about every single player in the game, and will be sent to every player conncted
-    var pack = [];
-    for(var i in PLAYER_LIST){
-        var player = PLAYER_LIST[i];
-        player.updatePosition();
-        pack.push({
-            x:player.x,
-            y:player.y,
-            number:player.number // player number added to package
-        });
-       
+    var pack = {
+        player:Player.update(),
     }
+
     // Server emits the pack to each  connected client
     for(var i in SOCKET_LIST){
         var socket = SOCKET_LIST[i];
