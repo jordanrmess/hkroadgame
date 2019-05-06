d13308017d401863b2f97d05092fd48115d16e23
Helena Rosenzweig <helena.rosenzw@gmail.com>
2019-05-06 19:24:42 +0200

    sprite

--- app.js
+++ app.js
@@ -2,12 +2,6 @@
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app); 
-// var MongoClient = require('mongodb').MongoClient;
-var bodyParser = require('body-parser');
-// var mongoose = require('mongoose');
-var mongojs = require("mongojs");
-var db= mongojs('localhost:27017/users',['users']);
-
 
 // If query is '/' (nothing)
 app.get('/',function(req,res){
@@ -17,28 +11,14 @@ app.get('/',function(req,res){
 // If query is '/client'. Client can only request things from client folder
 app.use('/client',express.static(__dirname + '/client')); 
 
-
-app.post('/api/user', function (req, res) {
-    var user = new User({
-        username: req.body.username,
-        password: req.body.password,
-        max_score: 0
-    });
-
-    user.save(function (err) {
-        if (err) throw err;
-        return res.send('Succesfully added user.');
-    });
-});
-
 // Server starts listening on port 2000
-serv.listen((process.env.PORT || 2000), () => {console.log("Server started");});
+serv.listen(2000);
+console.log("Server started");
 
 // Socket list keeps track of all clients connected to the server. 
 var SOCKET_LIST = {};
-
+var PLAYER_DIRECTIONS = {"up":3,"down":0,"right":2,"left":1};
 var AVAILABLE_PLAYERS = [1,2];
-var SOCKET_ID_TO_NUMBER = {};
 
 var Entity = function(){
     var self = {
@@ -61,7 +41,6 @@ var Entity = function(){
     return self;
 }
 
-
 // Create a player, passes id as parameter
 var Player = function(id) {
     var self = Entity();
@@ -74,11 +53,15 @@ var Player = function(id) {
     self.xDeathPos = 0;
     self.yDeathPos = 0;
     self.successCoords = []; 
+    self.walkingCounter = 0;
+    self.playerDirection = PLAYER_DIRECTIONS["right"];
+    self.walkingMod = self.walkingCounter%3;
     self.alive = true;
-    self.maxSpd = 10;
+    self.maxSpd = 5;
     self.count = 0;
 
     // Check which player, and give different spawning points
+
     self.setStartingPosition = function(){
        // self.alive = true;
         if(self.number === "1"){
@@ -93,6 +76,7 @@ var Player = function(id) {
     var super_update = self.update;
     
     self.update = function(){
+        self.walkingCounter ++;
         self.updateSpd();
         super_update();
 
@@ -106,23 +90,44 @@ var Player = function(id) {
     self.updateSpd = function(){
         if(self.pressingRight && self.x<680){
             self.spdX = self.maxSpd;
+            self.playerDirection = PLAYER_DIRECTIONS["right"];
+            self.walkingCounter += 0.001;
+            self.walkingMod = Math.floor(self.walkingCounter%3);
+
         }
         else if(self.pressingLeft && self.x>22){
             self.spdX = -self.maxSpd;
+            self.playerDirection = PLAYER_DIRECTIONS["left"];
+            self.walkingCounter += 0.001;
+            self.walkingMod = Math.floor(self.walkingCounter%3);
+
         }
         else{
             self.spdX=0;
+            if(self.playerDirection === PLAYER_DIRECTIONS["right"]){
+
+            }
         }
 
         if(self.pressingDown && self.y < 390){
             self.spdY = self.maxSpd;
+            self.playerDirection = PLAYER_DIRECTIONS["down"];
+            self.walkingCounter += 0.001;
+            self.walkingMod = Math.floor(self.walkingCounter%3);
+
         }
         else if(self.pressingUp && self.y > 25){
             self.spdY = -self.maxSpd;
+            self.playerDirection = PLAYER_DIRECTIONS["up"];
+            self.walkingCounter += 0.001;
+            self.walkingMod = Math.floor(self.walkingCounter%3);
+
         }
         else{
             self.spdY=0;
         }
+        //self.walkingMod ++;
+        console.log("WALKING MOD: ", self.walkingMod);
     }
     
     self.getInitPack = function(){
@@ -133,7 +138,10 @@ var Player = function(id) {
             alive:self.alive,
             number:self.number,
             count:self.count,
-            successCoords: self.successCoords
+            successCoords: self.successCoords,
+            walkingMod:self.walkingMod,
+            playerDirection: self.playerDirection,
+            walkingCounter: self.walkingCounter
 
         }
     }
@@ -148,8 +156,9 @@ var Player = function(id) {
             yDeathPos:self.yDeathPos,
             alive:self.alive,    
             count:self.count,
-            successCoords: self.successCoords
-
+            successCoords: self.successCoords,
+            walkingMod:self.walkingMod,
+            playerDirection:self.playerDirection
         }
     }
 
@@ -167,7 +176,6 @@ Player.list = {};
 Player.onConnect = function(socket){
     
     var player = Player(socket.id);
-    SOCKET_ID_TO_NUMBER[socket.id] = player.number;
 
     // Listens to client key presses, updates states of client accordingly
     socket.on('keyPress',function(data){
@@ -186,11 +194,8 @@ Player.onConnect = function(socket){
         // Server tells client "hey, you have this socket id"
         selfId:socket.id,
         player:Player.getAllInitPack(),
-        car: Car.getAllInitPack(),
-        game: currentGame.getInitPack()
+        car: Car.getAllInitPack()
     })
-    
-    Game.numConnections++;
 }
 
 Player.getAllInitPack = function(){
@@ -204,7 +209,6 @@ Player.getAllInitPack = function(){
 Player.onDisconnect = function(socket){
     delete Player.list[socket.id];
     removePack.player.push(socket.id);
-
 }
 
 Player.update = function(){
@@ -251,6 +255,7 @@ var Car = function(x,y, spdY, drivingDown){
                 p.setStartingPosition();
             }
         }
+        //super_update();
     }
     self.getInitPack = function(){
         return{
@@ -268,12 +273,17 @@ var Car = function(x,y, spdY, drivingDown){
         }
     }
 
+
+
     Car.list[self.id] = self;    
     initPack.car.push(self.getInitPack());
+
     return self; 
 
 }
 Car.list = {}; 
+
+
 
 Car.onConnect = function(){
     drivingDown = true;
@@ -286,12 +296,17 @@ Car.onConnect = function(){
     var car5 = Car(525.5, 0, 7, drivingDown);
     var car6 = Car(600.5, 0, 5, drivingDown);
 
+    //var car3 = Car();
+   // console.log("Car list: " + Car.list[car.id]);
 }
 Car.update = function(){
      var pack = [];
      for(var i in Car.list){
          var car = Car.list[i]; 
+      //   console.log("car.y BEFORE update ",car.y);
          car.update(); 
+      //   console.log("car.y AFTER update ",car.y);
+
 
          pack.push({
              id:car.id,
@@ -308,191 +323,38 @@ Car.getAllInitPack = function(){
     for(var i in Car.list){
         cars.push(Car.list[i].getInitPack());
     }
+ //   console.log(cars);
     return cars;
 }
- 
 
-//Callback functions mock database promises
-var isValidPassword = function(data,cb){
-    db.users.find({username:data.username,password:data.password},function(err,res){
-        if(res.length > 0)
-            cb(true);
-        else 
-            cb(false);
-    });
-}
-
-var userExists = function(data,cb){
-
-    db.users.find({username:data.username},function(err,res){
-        if(res.length >0){
-            cb(true);
-        }else{
-            cb(false);
-        }
-    });
-}
-
-var addUser = function(data,cb){
-    db.users.insert({username:data.username,password:data.password,score:0},function(err,res){
-        cb();
-    });
-
-}
-
-var getTopPlayers = function(cb){
-    var all_scores = [];
-    var top_scores = []; 
-    db.users.find(function(err,res){
-
-        console.log(res);
-        res.forEach(function (user) {
-            all_scores.push({username:user.username,score:user.score});
-        });
-        top_scores = all_scores.sort((a,b) => a.score<b.score).slice(0,3);
-        cb(top_scores);
-
-     });
-}
-    
+// Initializes an io Socket object 
 
 var io = require('socket.io')(serv,{}); 
-var currentGame;
-
-//Game object stores important information about the current game
-var Game = function(){
-
-    //Time starts at 30 seconds
-    var self = {
-        timeRemaining:5, 
-        numConnections:0
-    }
-   
-
-    self.startTimer = function() {
-        if(self.timeRemaining > 0){
-            self.timeRemaining-=1; 
-            setTimeout(self.startTimer,1000)
-        }else{
-            //Time has run out, alert the clients
-            io.emit("GAME_OVER",Player.update()); 
-        }
-        
-    }
-
-    self.getInitPack = function(){
-        return {
-            timeRemaining: self.timeRemaining,
-            numConnections: self.numConnections
-        }
-    }
-
-    self.getUpdatePack = function() {
-        return{
-            timeRemaining: self.timeRemaining,
-            numConnections: self.numConnections
-        }
-    }
-
-    initPack.game = (self.getInitPack()); 
-    return self; 
-}
-
-//Creating game object
-Game.init = function(){
-    currentGame = Game();
-}
-
-
 var maxConnections=2;
-var players_ready=0; 
-
+var currentConnections=0;
+// var car = Car();
 io.sockets.on('connection',function(socket){
-   console.log("client %s connected", socket.id);
-   if(currentGame.numConnections === maxConnections){
-        console.log("max connections");
-        socket.disconnect(true);
-        console.log("disconnect socket. number of connections are %s", currentgame.numConnections);
-    }
-    currentGame.numConnections ++;
-
     // server assigns a unique id to the socket
+    if(currentConnections === maxConnections){
+        socket.disconnect();
+    }
+
     socket.id=Math.random();
+    // Add it to the list of sockets currently online
     SOCKET_LIST[socket.id] = socket;
-
-    socket.on("SIGN_IN_REQUEST", function(data){
-        isValidPassword(data,function(res){
-            if(res){
-                Player.onConnect(socket);
-                socket.emit("SIGN_IN_RESPONSE",{success:true});
-            }else{
-                socket.emit("SIGN_IN_RESPONSE",{success:false});
-            }
-        })
-    });
-
-    //Adding a new user to mock database 
-    socket.on("SIGN_UP_REQUEST",function(data){
-        userExists(data,function(res){
-            if(res){
-                socket.emit("SIGN_UP_RESPONSE",{success:false});
-            }else{
-                addUser(data,function(res){
-                    socket.emit("SIGN_UP_RESPONSE",{success:true});
-                })
-            }
-        });
-    });
-
-    //If the game has 2 players, start game
-    socket.on("START_GAME",function(){ 
-        players_ready +=1; 
-       // console.log(players_ready);
-        if(players_ready ==2){
-            io.emit("GAME_STARTED");
-            currentGame.startTimer(io); 
-        }else{
-            socket.emit("START_RESPONSE", {ready:false}); 
-        }
-    }); 
-
-    socket.on("NEW_SCORE_REQUEST",function(data){
-        var current_socket_score=0;
-        var query = db.users.find({username:data.username});
-        query.toArray(function (err, docs) {
-            if(err){console.log("error accessing record" + err);}
-            docs.forEach(function (doc) {
-                current_socket_score = doc.score;
-            });
-              //if client has a new score
-            if(data.score>current_socket_score){
-                db.users.update({username:data.username},{username: data.username,password:data.password,score:data.score});
-                console.log("new high score for: " + data.username+ " of "  + data.score);
-            }
-            });
-    });
-
-    socket.on("CLIENT_LEADERBOARD_REQUEST",function(){
-        getTopPlayers(function(res){
-            socket.emit("SERVER_LEADERBOARD_RESPONSE",res);
-        });
-    })
-
+    Player.onConnect(socket);
+    currentConnections++;
+    
     // Server listens to disconnects, and removes disconnected clients.
     socket.on('disconnect',function(){
-        AVAILABLE_PLAYERS.push(SOCKET_ID_TO_NUMBER[socket.id]);
-        AVAILABLE_PLAYERS.sort();
-
         delete SOCKET_LIST[socket.id]; 
         Player.onDisconnect(socket);
-        currentGame.numConnections--;
-        
+        currentConnections--;
     });
-    //console.log("current connections: %s", currentGame.numConnections);
 });
 
-var initPack = {player:[],car:[],game:[]};
-var removePack = {player:[],game:[]};
+var initPack = {player:[],car:[]};
+var removePack = {player:[]};
 
 var initializeServer = true;
 
@@ -500,15 +362,12 @@ var initializeServer = true;
 setInterval(function(){
     // pack contains information about every single player in the game, and will be sent to every player conncted
     if(initializeServer){
-        //Init Game object 
-        Game.init();
         Car.onConnect(); 
         initializeServer = false;
     }
     var pack = {
         player:Player.update(),
-        car:Car.update(),
-        game:currentGame.getUpdatePack()
+        car:Car.update()
     }
     // Server emits the pack to each  connected client
     for(var i in SOCKET_LIST){
@@ -517,10 +376,9 @@ setInterval(function(){
         socket.emit("update",pack);
         socket.emit("remove", removePack);
     }
-
     initPack.player = [];
     initPack.car = []; 
-    initPack.game = []; 
     removePack.player = [];
+
 
 },1000/25);
